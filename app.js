d7208b28e0851f1bd2e2db9f6890ba0151904676
MuhammadAsaad2001 <MuhammadAsad19B@gmail.com>
2020-06-28 01:16:36 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+function getnumber(num){
+   
+    var result = document.getElementById("result")
+    (result.value += num;
+
+}
+
+function clearresult(num){
+   
+    var result = document.getElementById("result")
+    result.value = "";
+
+}
+function getresult(){
+    var result = document.getElementById("result");
+result.value = eval(result.value);
+}
